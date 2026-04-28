83e87990b78475c1d36a125d3f63e6aabfe84a54
Mario Reder <mario.reder@pm.me>
2026-04-28 23:00:19 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -18,7 +18,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_APP_DESCRIPTION": "mhehe but perps DEX meta",
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
-  "VITE_ENABLED_MENUS": "Trading,Portfolio,Markets",
+  "VITE_ENABLED_MENUS": "Trading,Portfolio,Markets,Campaigns",
   "VITE_CUSTOM_MENUS": "",
   "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "true",
   "VITE_ENABLE_CAMPAIGNS": "false",
